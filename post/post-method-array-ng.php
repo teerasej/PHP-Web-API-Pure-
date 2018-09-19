ae59a5dcffbb2c610030bacd66f528a6bfc972ee
Teerasej Jiraphatchandej <teerasej@gmail.com>
2018-09-19 08:34:52 +0700

    Support CORS

--- post/post-method-array-ng.php
+++ post/post-method-array-ng.php
@@ -1,7 +1,8 @@
 <?php
-
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
 // Get first and second parameter from client
 
