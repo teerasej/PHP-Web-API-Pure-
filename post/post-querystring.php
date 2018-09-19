c82303192686deeb55d147f02c5f950e8c39f67e
Teerasej Jiraphatchandej <teerasej@gmail.com>
2018-09-19 08:35:51 +0700

    Support CORS

--- post/post-querystring.php
+++ post/post-querystring.php
@@ -1,5 +1,8 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET,HEAD,OPTIONS,POST,PUT");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
 $result = "Thank you. We got your info: ";
 
